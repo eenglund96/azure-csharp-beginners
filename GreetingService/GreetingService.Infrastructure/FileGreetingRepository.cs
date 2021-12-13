d3a03660f2c8dc451fea031746c803e84b2fc426
Keen Fann <KeenFann@users.noreply.github.com>
2021-12-13 09:43:06 +0100

    Implemented file repo

---
+++ GreetingService/GreetingService.Infrastructure/FileGreetingRepository.cs
@@ -0,0 +1,68 @@
+﻿using GreetingService.Core.Entities;
+using GreetingService.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GreetingService.Infrastructure
+{
+    public class FileGreetingRepository : IGreetingRepository
+    {
+        private readonly string _filePath = "greeting.json";
+        private readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions { WriteIndented = true, };
+
+        public FileGreetingRepository(string filePath)
+        {
+            if (!File.Exists(_filePath))
+                File.WriteAllText(_filePath, "[]");     //init file with empty json array
+
+            _filePath = filePath;
+        }
+
+        public void Create(Greeting greeting)
+        {
+            var content = File.ReadAllText(_filePath);
+            var greetings = JsonSerializer.Deserialize<IList<Greeting>>(content);
+
+            if (greetings.Any(x => x.Id == greeting.Id))
+                throw new Exception($"Greeting with id: {greeting.Id} already exists");
+
+            greetings.Add(greeting);
+
+            File.WriteAllText(_filePath, JsonSerializer.Serialize(greetings, _jsonSerializerOptions));
+        }
+
+        public Greeting Get(Guid id)
+        {
+            var content = File.ReadAllText(_filePath);
+            var greetings = JsonSerializer.Deserialize<IList<Greeting>>(content);
+            return greetings?.FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<Greeting> Get()
+        {
+            var content = File.ReadAllText(_filePath);
+            var greetings = JsonSerializer.Deserialize<IList<Greeting>>(content);
+            return greetings;
+        }
+
+        public void Update(Greeting greeting)
+        {
+            var content = File.ReadAllText(_filePath);
+            var greetings = JsonSerializer.Deserialize<IList<Greeting>>(content);
+            var existingGreeting = greetings.FirstOrDefault(x => x.Id == greeting.Id);
+
+            if (existingGreeting == null)
+                throw new Exception($"Greeting with id: {greeting.Id} not found");
+
+            existingGreeting.To = greeting.To;
+            existingGreeting.From = greeting.From;
+            existingGreeting.Message = greeting.Message;
+
+            File.WriteAllText(_filePath, JsonSerializer.Serialize(greetings, _jsonSerializerOptions));
+        }
+    }
+}
