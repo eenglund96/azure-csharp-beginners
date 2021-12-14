4ceca5cf48fc02571f76e17eeb6421553bf67812
Keen Fann <KeenFann@users.noreply.github.com>
2021-12-14 10:59:03 +0100

    Added missing files

---
+++ GreetingService/GreetingService.API/Controllers/GreetingController.cs
@@ -0,0 +1,59 @@
+﻿using GreetingService.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GreetingService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreetingController : ControllerBase
+    {
+        // GET: api/<GreetingController>
+        [HttpGet]
+        public IEnumerable<Greeting> Get()
+        {
+            return new Greeting[]
+            {
+                new Greeting
+                {
+                    From = "Keen",
+                    To = "Anton",
+                    Message = "Hello",
+                },
+                new Greeting
+                {
+                    From = "Keen",
+                    To = "Anton",
+                    Message = "Hello again",
+                },
+            };
+        }
+
+        // GET api/<GreetingController>/5
+        [HttpGet("{id}")]
+        public Greeting Get(Guid id)
+        {
+            return new Greeting
+            {
+                From = "Keen",
+                To = "Anton",
+                Message = "Hello",
+            };
+        }
+
+        // POST api/<GreetingController>
+        [HttpPost]
+        public void Post([FromBody] Greeting greeting)
+        {
+            Console.WriteLine($"Create: {greeting.Message}");
+        }
+
+        // PUT api/<GreetingController>
+        [HttpPut]
+        public void Put([FromBody] Greeting greeting)
+        {
+            Console.WriteLine($"Update: {greeting.Message}");
+        }
+    }
+}
---
+++ GreetingService/GreetingService.Core/Entities/Greeting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingService.Core.Entities
+{
+    public class Greeting
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Message { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public DateTime Timestamp { get; } = DateTime.Now;
+    }
+}
