ad60436c2c05381ebb8f7eea5c4b636f0202f2d0
Keen Fann <KeenFann@users.noreply.github.com>
2021-12-15 10:28:21 +0100

    Implemented http client that calls greeting service

---
+++ GreetingService/GreetingService.API.Client/Greeting.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingService.API.Client
+{
+    /// <summary>
+    /// Create this class with Edit->Paste Special->Paste JSON As Classes
+    /// When writing a client for a REST API we typically don't have access to the source code of the service.
+    /// To make it easier to interact with the service we need to create our own local representation of the contract (request and response messages)
+    /// A common way to construct these classes is to use `Paste JSON as Classes` in Visual Studio
+    /// Paste JSON as classes will try to infer the type of each property from values in the JSON in a best effort manner. 
+    /// Be sure to always check that the types are what we want them to be.
+    /// </summary>
+    public class Greeting                       //Always rename the default name "Rootobject" to the correct class name
+    {
+        /// <summary>
+        /// Paste JSON as classes will think this is a string and that's fine for most cases. But for our application, change this to a GUID
+        /// </summary>
+        public Guid id { get; set; } = Guid.NewGuid();      //generate guid as default
+        public string message { get; set; }
+        public string from { get; set; }
+        public string to { get; set; }
+        public DateTime timestamp { get; set; }
+    }
+
+}
---
+++ GreetingService/GreetingService.API.Client/Program.cs
@@ -0,0 +1,178 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace GreetingService.API.Client;
+
+public class GreetingServiceClient
+{
+    private static HttpClient _httpClient = new();                          //always reuse http client to avoid exhausting network resources. A static field is shared by all instances of a class
+
+    private const string _getGreetingsCommand = "get greetings";
+    private const string _getGreetingCommand = "get greeting ";
+    private const string _writeGreetingCommand = "write greeting ";
+    private const string _updateGreetingCommand = "update greeting ";
+    private static string _from = "Batman";
+    private static string _to = "Superman";
+
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("Welcome to command line Greeting client");
+        Console.WriteLine("Enter name of greeting sender:");
+        var from = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(from))
+            _from = from;
+
+        Console.WriteLine("Enter name of greeting recipient:");
+        var to = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(to))
+            _to = to;
+
+        while (true)
+        {
+            Console.WriteLine("Available commands:");
+            Console.WriteLine(_getGreetingsCommand);
+            Console.WriteLine($"{_getGreetingCommand} [id]");
+            Console.WriteLine($"{_writeGreetingCommand} [message]");
+            Console.WriteLine($"{_updateGreetingCommand} [id] [message]");
+
+            Console.WriteLine("\nWrite command and press [enter] to execute");
+
+            var command = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(command))
+            {
+                Console.WriteLine("Command cannot be empty\n");
+                continue;
+            }
+
+            if (command.Equals(_getGreetingsCommand, StringComparison.OrdinalIgnoreCase))
+            {
+                await GetGreetingsAsync();
+            }
+            else if (command.StartsWith(_getGreetingCommand, StringComparison.OrdinalIgnoreCase))
+            {
+                var idPart = command.Replace(_getGreetingCommand, "");
+                if (Guid.TryParse(idPart, out var id))
+                {
+                    await GetGreetingAsync(id);
+                }
+                else
+                {
+                    Console.WriteLine($"{idPart} is not a valid GUID\n");
+                }
+            }
+            else if (command.StartsWith(_writeGreetingCommand, StringComparison.OrdinalIgnoreCase))
+            {
+                var message = command.Replace(_writeGreetingCommand, "");
+                await WriteGreetingAsync(message);
+            }
+            else if (command.StartsWith(_updateGreetingCommand, StringComparison.OrdinalIgnoreCase))
+            {
+                var idAndMessagePart = command.Replace(_updateGreetingCommand, "") ?? "";
+                var idPart = idAndMessagePart.Split(" ").First();
+                var messagePart = idAndMessagePart.Replace(idPart, "").Trim();
+
+                if (Guid.TryParse(idPart, out var id))
+                {
+                    await UpdateGreetingAsync(id, messagePart);
+                }
+                else
+                {
+                    Console.WriteLine($"{idPart} is not a valid GUID");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Command not recognized\n");
+            }
+        }
+    }
+
+    /// <summary>
+    /// async key word means this method is awaitable. Return type Task is equivalent to void in a sync (normal) method
+    /// IO operations (network, disk, db, etc) are slow. Using await async allows the CPU to work on other operations while waiting for the async operation to return.
+    /// In general use async methods if possible and follow the principle of "async all the way"
+    /// </summary>
+    /// <returns></returns>
+    private static async Task GetGreetingsAsync()
+    {
+        try
+        {
+            var response = await _httpClient.GetAsync("http://localhost:5020/api/greeting");
+            response.EnsureSuccessStatusCode();                                                 //throws exception if HTTP response status is not a success status
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            //Do something with response
+            var greetings = JsonSerializer.Deserialize<IEnumerable<Greeting>>(responseBody);
+
+            foreach (var greeting in greetings)
+            {
+                Console.WriteLine($"[{greeting.id}] [{greeting.timestamp}] ({greeting.from} -> {greeting.to}) - {greeting.message}");
+            }
+
+            Console.WriteLine();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Get greetings failed: {e.Message}\n");
+        }
+    }
+
+    private static async Task GetGreetingAsync(Guid id)
+    {
+        try
+        {
+            var response = await _httpClient.GetAsync($"http://localhost:5020/api/greeting/{id}");
+            response.EnsureSuccessStatusCode();
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            //Do something with response;
+            var greeting = JsonSerializer.Deserialize<Greeting>(responseBody);
+            Console.WriteLine($"[{greeting.id}] [{greeting.timestamp}] ({greeting.from} -> {greeting.to}) - {greeting.message}\n");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Get greeting failed: {e.Message}\n");
+        }
+    }
+
+    private static async Task WriteGreetingAsync(string message)
+    {
+        try
+        {
+            var greeting = new Greeting
+            {
+                from = _from,
+                to = _to,
+                message = message,
+            };
+            var response = await _httpClient.PostAsJsonAsync("http://localhost:5020/api/greeting", greeting);
+            Console.WriteLine($"Wrote greeting. Service responded with: {response.StatusCode}");            //all HTTP responses always contain a status code
+            Console.WriteLine();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Write greeting failed: {e.Message}\n");
+        }
+    }
+
+    private static async Task UpdateGreetingAsync(Guid id, string message)
+    {
+        try
+        {
+            var greeting = new Greeting
+            {
+                id = id,
+                from = _from,
+                to = _to,
+                message = message,
+            };
+            var response = await _httpClient.PutAsJsonAsync("http://localhost:5020/api/greeting", greeting);
+            Console.WriteLine($"Updated greeting. Service responded with: {response.StatusCode}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Update greeting failed: {e.Message}\n");
+        }
+    }
+}
