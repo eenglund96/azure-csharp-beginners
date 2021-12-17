4434d116a9fabda1735368935dfbf119c5df3581
Keen Fann <KeenFann@users.noreply.github.com>
2021-12-17 11:41:33 +0100

    Revert "Revert "Added app insights logging""
    This reverts commit 152f6d5dfac134277c2428ef19b5718d3422febf.

--- GreetingService/GreetingService.Infrastructure/AppSettingsUserService.cs
+++ GreetingService/GreetingService.Infrastructure/AppSettingsUserService.cs
@@ -1,5 +1,6 @@
 ﻿using GreetingService.Core.Interfaces;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,18 +12,27 @@ namespace GreetingService.Infrastructure
     public class AppSettingsUserService : IUserService
     {
         private readonly IConfiguration _configuration;
+        private readonly ILogger<AppSettingsUserService> _logger;
 
-        public AppSettingsUserService(IConfiguration configuration)
+        public AppSettingsUserService(IConfiguration configuration, ILogger<AppSettingsUserService> logger)
         {
             _configuration = configuration;
+            _logger = logger;
         }
 
         public bool IsValidUser(string username, string password)
         {
             var entries = _configuration.AsEnumerable().ToDictionary(x => x.Key, x => x.Value);
             if (entries.TryGetValue(username, out var storedPassword))
-                return storedPassword == password;
+            {
+                if (storedPassword == password)
+                {
+                    _logger.LogInformation("Valid credentials for {username}", username);
+                    return true;
+                }
+            }
 
+            _logger.LogWarning("Invalid credentials for {username}", username);
             return false;
         }
     }
