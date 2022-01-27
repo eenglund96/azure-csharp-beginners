96d216666d10cc9ac26414fe5c991508188c4d51
Keen Fann <KeenFann@users.noreply.github.com>
2022-01-27 14:49:33 +0100

    Added serilog and log to storage account. Also added storage account to bicep

--- GreetingService/GreetingService.API.Function/Startup.cs
+++ GreetingService/GreetingService.API.Function/Startup.cs
@@ -4,6 +4,9 @@ using GreetingService.Infrastructure;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using System;
+using Serilog;
 
 [assembly: FunctionsStartup(typeof(GreetingService.API.Function.Startup))]
 namespace GreetingService.API.Function
@@ -12,15 +15,31 @@ namespace GreetingService.API.Function
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            builder.Services.AddHttpClient();
+            var config = builder.GetContext().Configuration;        //Get all configured app settings in an IConfiguration like this
 
+            builder.Services.AddHttpClient();
             builder.Services.AddLogging();
-            
-            builder.Services.AddScoped<IGreetingRepository, FileGreetingRepository>(c =>
+
+            //Create a Serilog logger and register it as a logger
+            //Get the Azure Storage Account connection string from our IConfiguration
+            builder.Services.AddLogging(c => 
             {
-                var config = c.GetService<IConfiguration>();
-                return new FileGreetingRepository(config["FileRepositoryFilePath"]);
+                var connectionString = config["LoggingStorageAccount"];
+                if (string.IsNullOrWhiteSpace(connectionString))
+                    return;
+
+                var logName = $"{Assembly.GetCallingAssembly().GetName().Name}.log";
+                var logger = new LoggerConfiguration()
+                                    .WriteTo.AzureBlobStorage(connectionString,
+                                                              restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+                                                              storageFileName: "{yyyy}/{MM}/{dd}/" + logName,
+                                                              outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] [{SourceContext}] {Message}{NewLine}{Exception}")
+                                    .CreateLogger();
+
+                c.AddSerilog(logger, true);
             });
+
+            builder.Services.AddSingleton<IGreetingRepository, MemoryGreetingRepository>();
 
             builder.Services.AddScoped<IUserService, AppSettingsUserService>();
 
--- azure_resources.bicep
+++ azure_resources.bicep
@@ -3,12 +3,22 @@ param location string = resourceGroup().location
 
 // storage accounts must be between 3 and 24 characters in length and use numbers and lower-case letters only
 var storageAccountName = '${substring(appName,0,10)}${uniqueString(resourceGroup().id)}' 
+var loggingStorageAccountName = '${substring(appName,0,7)}log${uniqueString(resourceGroup().id)}' 
 var hostingPlanName = '${appName}${uniqueString(resourceGroup().id)}'
 var appInsightsName = '${appName}${uniqueString(resourceGroup().id)}'
 var functionAppName = '${appName}'
 
 resource storageAccount 'Microsoft.Storage/storageAccounts@2019-06-01' = {
   name: storageAccountName
+  location: location
+  kind: 'StorageV2'
+  sku: {
+    name: 'Standard_LRS'
+  }
+}
+
+resource loggingStorageAccount 'Microsoft.Storage/storageAccounts@2019-06-01' = {
+  name: loggingStorageAccountName
   location: location
   kind: 'StorageV2'
   sku: {
@@ -58,6 +68,10 @@ resource functionApp 'Microsoft.Web/sites@2020-06-01' = {
           value: 'DefaultEndpointsProtocol=https;AccountName=${storageAccount.name};EndpointSuffix=${environment().suffixes.storage};AccountKey=${listKeys(storageAccount.id, storageAccount.apiVersion).keys[0].value}'
         }
         {
+          name: 'LoggingStorageAccount'
+          value: 'DefaultEndpointsProtocol=https;AccountName=${loggingStorageAccount.name};EndpointSuffix=${environment().suffixes.storage};AccountKey=${listKeys(loggingStorageAccount.id, loggingStorageAccount.apiVersion).keys[0].value}'
+        }
+        {
           'name': 'FUNCTIONS_EXTENSION_VERSION'
           'value': '~4'
         }
@@ -68,6 +82,10 @@ resource functionApp 'Microsoft.Web/sites@2020-06-01' = {
         {
           name: 'WEBSITE_CONTENTAZUREFILECONNECTIONSTRING'
           value: 'DefaultEndpointsProtocol=https;AccountName=${storageAccount.name};EndpointSuffix=${environment().suffixes.storage};AccountKey=${listKeys(storageAccount.id, storageAccount.apiVersion).keys[0].value}'
+        }
+        {
+          name: 'FileRepositoryFilePath'
+          value: '/home/site/wwwroot/greeting.json'
         }
         // WEBSITE_CONTENTSHARE will also be auto-generated - https://docs.microsoft.com/en-us/azure/azure-functions/functions-app-settings#website_contentshare
         // WEBSITE_RUN_FROM_PACKAGE will be set to 1 by func azure functionapp publish
